ccec9fde10a52e73b5c89b3809bbcf1823924384
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-10-12 19:41:07 +0200

    feat: send confirmation mail to user

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,48 +11,51 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 interface Body {
-    subject: string;
-    message: string;
-    email: string;
-    name: string;
+  message: string;
+  email: string;
+  name: string;
 }
 
 app.post("/", async (req: Request<{}, {}, Body>, res: Response) => {
-    try {
-        const { subject, message, email, name } = req.body;
-        if (!subject || !message || !email || !name) {
-            res.status(400).json({ message: "Missing required fields" });
-            return;
-        }
-        const transporter = nodemailer.createTransport({
-            service: "Gmail",
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.USERNAME,
-                pass: process.env.PASSWORD,
-            },
-        });
-        const text = `${message}\n\nUpit šalje:\n${name},\n${email}`;
-        const mail = {
-            from: process.env.USERNAME,
-            to: process.env.USERNAME,
-            subject: subject,
-            text: text,
-        };
-        const info = await transporter.sendMail(mail);
-        console.log("Email sent:", info.response);
-        res.status(200).json({ message: "Email sent successfully" });
-    } catch (error) {
-        console.error("Error sending email: ", error);
+  try {
+    const { message, email, name } = req.body;
+    if (!message || !email || !name) {
+      res.status(400).json({ message: "Missing required fields" });
+      return;
     }
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.USERNAME,
+        pass: process.env.PASSWORD,
+      },
+    });
+    const adminMail = {
+      from: process.env.USERNAME,
+      to: process.env.USERNAME,
+      subject: `Novi mail od ${name}, ${email}`,
+      text: `${message}\n\nUpit šalje:\n${name},\n${email}`,
+    };
+    const confirmationMail = {
+      from: process.env.USERNAME,
+      to: email,
+      subject: "Primili smo vas upit",
+      text: `Pozdrav ${name},\n\nPrimili smo vas upit i odgovorit cemo vam u najkracem mogucem roku.\n\nBaloncici`,
+    };
+    await Promise.all([transporter.sendMail(adminMail), transporter.sendMail(confirmationMail)]);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email: ", error);
+  }
 });
 
 app.use("*", (req: Request, res: Response) => {
-    res.status(404).json({ message: "Error: invalid endpoint" });
+  res.status(404).json({ message: "Error: invalid endpoint" });
 });
 
 app.listen(PORT, () => {
-    console.log(`App is live at port ${PORT}`);
+  console.log(`App is live at port ${PORT}`);
 });
